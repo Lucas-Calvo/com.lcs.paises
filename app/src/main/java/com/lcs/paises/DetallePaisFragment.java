f5d9540f829115ec6ae66f111e4f46b2985b50dd
Lucas-Calvo <lucascalvo@hotmail.es>
2021-11-21 11:40:06 +0100

    Initial commit

---
+++ app/src/main/java/com/lcs/paises/DetallePaisFragment.java
@@ -0,0 +1,33 @@
+package com.lcs.paises;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DetallePaisFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DetallePaisFragment extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detalle_pais, container, false);
+    }
+}
